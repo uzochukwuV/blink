1bcf924516cbe3aed7b98c3cbebcdd47e2cadb95
visualise crypto <vic.ezealor@gmail.com>
2025-08-29 13:10:07 +0000

    feat: update bet retrieval to support on-chain fetching and remove deprecated POST endpoint
    Co-authored-by: Genie <genie@cosine.sh>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '~/app/providers';
 import { APP_NAME, APP_DESCRIPTION } from '~/lib/constants';
-import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -19,15 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js" strategy="beforeInteractive" />
-      <Script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" strategy="beforeInteractive" />
-      <style type=' type="text/tailwindcss"'>{`
-        @theme {
-        --color-clifford: #da373d;
-      }
-        @import "tailwindcss";
-      `}</style>
-      <body className={inter.className + " bg-bg  text-textPrimary"}>
+      <body className={inter.className + " bg-background text-foreground"}>
         <Providers>
           {children}
         </Providers>
